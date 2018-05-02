3ec411be8ffcb10eec3b92b70f6001261a722595
handler <314173730@qq.com>
2018-05-02 18:17:02 +0800

    提交前三个练习题

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
@@ -3,7 +3,9 @@ package com.hencoder.hencoderpracticedraw2.practice;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -23,14 +25,19 @@ public class Practice01LinearGradientView extends View {
         super(context, attrs, defStyleAttr);
     }
 
-    {
-        // 用 Paint.setShader(shader) 设置一个 LinearGradient
-        // LinearGradient 的参数：坐标：(100, 100) 到 (500, 500) ；颜色：#E91E63 到 #2196F3
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        Shader shader = new LinearGradient(100, 100, 500, 500,
+                Color.parseColor("#E91E63"), Color.parseColor("#2196F3"), Shader.TileMode.CLAMP);
+        // 设置两个点和两种颜色，以这两个点作为端点，使用两种颜色的渐变来绘制颜色
+
+        //TileMode 一共有 3 个值可选： CLAMP, MIRROR 和 REPEAT.CLAMP  会在端点之外延续端点处的颜色；
+        // MIRROR 是镜像模式
+        // REPEAT 是重复模式。
+
+        paint.setShader(shader);
 
         canvas.drawCircle(300, 300, 200, paint);
     }
